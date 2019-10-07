f4bf6c109629a8ae94ee7a084a1dc6ddf36ab9cf
CheeseJedi <CheeseJedi@users.noreply.github.com>
2019-10-07 21:00:29 +0100

    Added two abstract classes - VariableChangedListenerObject (MonoBehaviour) and VariableChangedListenerSystem(ScriptableObjectSystem).

---
+++ Assets/SO Architecture/Events/Listeners/VariableChangedListenerSystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjectArchitecture
+{
+    public abstract class VariableChangedListenerSystem<TVar, TEvent>
+        : ScriptableObjectSystem, IGameEventListener
+        where TVar : BaseVariable
+        where TEvent : GameEventBase
+    {
+        protected ScriptableObject GameEvent => Variable as TEvent;
+        [Header("Monitored Variable")]
+        [Tooltip("The variable to monitor for changes.")]
+        [SerializeField]
+        protected TVar Variable = default;
+        [SerializeField, HideInInspector]
+        private TVar _previouslyRegisteredEvent = default;
+        /// <summary>
+        /// Called by the variable when it changes.
+        /// </summary>
+        public abstract void OnEventRaised();
+        protected virtual void OnEnable()
+        {
+            if (Variable != null)
+                Register();
+        }
+        private void OnDisable()
+        {
+            if (Variable != null)
+                Variable.RemoveListener(this);
+        }
+        private void Register()
+        {
+            if (_previouslyRegisteredEvent != null)
+            {
+                _previouslyRegisteredEvent.RemoveListener(this);
+            }
+            Variable.AddListener(this);
+            _previouslyRegisteredEvent = Variable;
+        }
+    }
+}
