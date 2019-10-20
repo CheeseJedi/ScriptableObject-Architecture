b5a19b57fa3e31c465fbfb4c19c9f3011a08c957
CheeseJedi <CheeseJedi@users.noreply.github.com>
2019-10-20 02:19:28 +0100

    Moved IPersistable from core library.

---
+++ Runtime/Systems/Persistence/IPersistable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjectArchitecture
+{
+    public interface IPersistable
+    {
+        string PersistenceGuid { get; }
+        System.Type Type { get; }
+        bool HasChildObjects { get; }
+        bool PersistenceEnabled { get; }
+        string Serialise();
+        void Deserialise(string input);
+    }
+}
