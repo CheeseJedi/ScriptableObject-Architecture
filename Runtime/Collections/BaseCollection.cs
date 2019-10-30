e8892075da4b4a97d3fe6700a7fc2cbc533bed5d
CheeseJedi <CheeseJedi@users.noreply.github.com>
2019-10-30 15:34:36 +0000

    BaseCollection updates: Implements IList rather than just IEnumerable. Add and Remove methods adjusted to fit different signatures.

--- Runtime/Collections/BaseCollection.cs
+++ Runtime/Collections/BaseCollection.cs
@@ -4,7 +4,7 @@ using Type = System.Type;
 
 namespace ScriptableObjectArchitecture
 {
-    public abstract class BaseCollection : GameEventBase, IEnumerable
+    public abstract class BaseCollection : GameEventBase, IList
     {
         public object this[int index]
         {
@@ -57,7 +57,7 @@ namespace ScriptableObjectArchitecture
                 }
             }
         }
-        public int Count { get { return List.Count; } }
+        public int Count => List.Count;
         protected abstract IList List { get; }
         public abstract Type Type { get; }
         /// <summary>
@@ -66,37 +66,40 @@ namespace ScriptableObjectArchitecture
         /// </summary>
         public virtual bool IsAutoSorted => false;
         public virtual bool IsReadOnly => false;
+
+        public bool IsFixedSize => List.IsFixedSize;
+        public bool IsSynchronized => List.IsSynchronized;
+        public object SyncRoot => List.SyncRoot;
+
         private const string DEFAULT_DEVELOPER_DESCRIPTION = "Default description for a Collection. Click to edit.";
+#pragma warning disable IDE0051 // Remove unused private members
         private void Awake()
+#pragma warning restore IDE0051 // Remove unused private members
         {
             if (DeveloperDescription == BASE_DEFAULT_DEVELOPER_DESCRIPTION)
                 DeveloperDescription = new DeveloperDescription(DEFAULT_DEVELOPER_DESCRIPTION);
         }
-        public void Add(object obj)
+        public int Add(object obj)
         {
             if (!List.Contains(obj))
             {
-                List.Add(obj);
+                int result = List.Add(obj);
                 if (IsAutoSorted)
                 {
                     Sort();
                 }
                 Raise();
+                return result;
             }
+            return -1;
         }
-        public bool Remove(object obj)
+        public void Remove(object obj)
         {
             if (List.Contains(obj))
             {
                 List.Remove(obj);
-                if (IsAutoSorted)
-                {
-                    Sort();
-                }
                 Raise();
-                return true;
             }
-            return false;
         }
         public void Clear()
         {
@@ -104,25 +107,15 @@ namespace ScriptableObjectArchitecture
             _selectedItemIndex = -1;
             Raise();
         }
-        public bool Contains(object obj)
-        {
-            return List.Contains(obj);
-        }
-        public int IndexOf(object obj)
-        {
-            return List.IndexOf(obj);
-        }
+        public bool Contains(object obj) => List.Contains(obj);
+        public int IndexOf(object obj) => List.IndexOf(obj);
         public void RemoveAt(int index)
         {
-            if (index == _selectedItemIndex)
+            if (_selectedItemIndex == index)
             {
                 SelectedItemIndex--;
             }
             List.RemoveAt(index);
-            if (IsAutoSorted)
-            {
-                Sort();
-            }
             Raise();
         }
         public void Insert(int index, object obj)
@@ -130,14 +123,9 @@ namespace ScriptableObjectArchitecture
             List.Insert(index, obj);
             Raise();
         }
-        public void CopyTo(object[] array, int arrayIndex)
-        {
-            List.CopyTo(array, arrayIndex);
-        }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return List.GetEnumerator();
-        }
+        public void CopyTo(object[] array, int index) => List.CopyTo(array, index);
+        public void CopyTo(System.Array array, int index) => List.CopyTo(array, index);
+        IEnumerator IEnumerable.GetEnumerator() => List.GetEnumerator();
         /// <summary>
         /// Sort method stub - to be overridden by derived collections that
         /// need to have their contents sorted when an item is added or removed.
@@ -162,13 +150,8 @@ namespace ScriptableObjectArchitecture
             int nextIndex = (reverse ? List.Count + currentIndex - 1 : currentIndex + 1) % List.Count;
             return List[nextIndex];
         }
-        public override string ToString()
-        {
-            return "BaseCollection<object>(" + Count + ")";
-        }
-        public virtual string GetNameOfItem(int index)
-        {
-            return $"[{index:00}]_{List[index].ToString()}";
-        }
+        public override string ToString() => "BaseCollection<object>(" + Count + ")";
+        public virtual string GetNameOfItem(int index) => $"[{index:00}]_{List[index].ToString()}";
+
     }
 }
